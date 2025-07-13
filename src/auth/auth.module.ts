b1565c75b779216ad45a1900cdcb452b84f69a94
ihorhnennyi <ihorhnennyi@gmail.com>
2025-07-13 14:32:56 +0300

    feat: подключена MongoDB, добавлены схемы и DTO для пользователей и авторизации

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { UsersModule } from '../users/users.module'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+
+@Module({
+	imports: [UsersModule, PassportModule, JwtModule.register({})],
+	controllers: [AuthController],
+	providers: [AuthService],
+})
+export class AuthModule {}
